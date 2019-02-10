f423fd669168669db1b9e7c7897ce5e9c9cb6b3e
AngelMD55 <angel_demerutis@hotmail.com>
2019-02-09 19:56:17 -0700

    fixed connection bug

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,28 @@
-// Set up MYSQL connection
-const mysql = require("mysql");
-const passwordFile = require("../password.js");
-const mysqlPassword = process.env.password;
-let connection;
+var mysql = require("mysql");
+//connected to my localhost db via mySQL workshop and NPM package
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
+} else {
     connection = mysql.createConnection({
-    host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "nqknq639d7vbfc0d",
-    password: "x4ua61z595l63ztn",
-    database: "xgv6yr9tnfq5h3mh"
+        host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "nqknq639d7vbfc0d",
+        password: "x4ua61z595l63ztn",
+        database: "xgv6yr9tnfq5h3mh"
+
+    });
+}
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return err;
+    }
+    console.log("Connection id: " + connection.threadId);
 });
 
-//Make Connection
-connection.connect(function(err){
-    if (err){
-        console.log("error connection: " + err.stack);
-        return;
-    }
-    console.log("connected ad id " + connection.threadId);
-})
+//if my connection was successful console log  the thread id
 
 module.exports = connection;
